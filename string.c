b1a1bb7f3fd87f8e264250074af5cf71e3525f3f
dbhowal <daipayan.bhowal@gmail.com>
2021-05-01 22:31:59 +0530

    strncat, strncpy and strtok added
    Written a feature for strncat, strncpy and strtok

--- string.c
+++ string.c
@@ -1,3 +1,73 @@
+char* strncat(char* destination, const char* source, size_t num)
+{
+    char *ptr = destination + strlen( destination);
+ 
+    while(*source != '\0' && num--)
+        *ptr++ = *source++;
+
+  *ptr = '\0';
+  
+   return destination;
+}
+
+char *strncpy(char *destination, const char *source, int num)
+{
+    int i = 0;
+     while(source[i] != '\0' && i < num)
+     {
+          destination[i] = source[i];
+          i++;
+      }
+      if(i == num)
+     {
+       destination[i] = '\0';
+      }
+   return destination;
+}
+
+char *strtok(char *source, char *delim)
+{
+   static char *next;
+   int i = 0,j = 0, flag1 = 0, flag2 = 0;
+   char *dup;
+   if(source == NULL)
+   {
+     source = next;
+   }
+   if (source[i] == '\0')
+   {
+     return NULL;
+   }
+
+   for (i = 0; source[i] != '\0'; i++)
+   {
+      for (j = 0; delim[j] != '\0'; j++)
+      {
+         if (source[i] == delim[j])
+         {
+           dup =(char*) malloc(sizeof(char)*(i));
+           my_strncpy(dup,source,i);
+           dup[i] = '\0';
+           i++;
+           j = -1;
+           flag1 = 1;
+         }
+         else
+         {
+           flag2 = 1;
+         }
+      }
+
+      if (flag1 && flag2)
+      {
+         next = source + i;
+         return dup;
+      }
+    }
+ next = source + i;
+ return source;
+}
+
 void strcat(char s[],char t[])
 {
     int i,j;
